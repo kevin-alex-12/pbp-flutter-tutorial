c2059fa075c3190b769cd97e6c6cd9c694388015
Kevin Alexander <kevinalexande26@gmail.com>
2022-11-17 09:54:55 +0700

    add lab08

--- lib/page/form.dart
+++ lib/page/form.dart
@@ -1,4 +1,5 @@
 import 'package:tesapp/main.dart';
+import 'package:tesapp/page/to_do_page.dart';
 import 'package:flutter/material.dart';
 
 class MyFormPage extends StatefulWidget {
@@ -25,6 +26,43 @@ class _MyFormPageState extends State<MyFormPage> {
       return Scaffold(
           appBar: AppBar(
               title: Text('Form'),
+          ),
+          drawer: Drawer(
+            child: Column(
+              children: [
+                // Menambahkan clickable menu
+                ListTile(
+                  title: const Text('Counter'),
+                  onTap: () {
+                    // Route menu ke halaman utama
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => const MyHomePage()),
+                    );
+                  },
+                ),
+                ListTile(
+                  title: const Text('Form'),
+                  onTap: () {
+                    // Route menu ke halaman form
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => const MyFormPage()),
+                    );
+                  },
+                ),
+                ListTile(
+                  title: const Text('To Do'),
+                  onTap: () {
+                    // Route menu ke halaman to do
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => const ToDoPage()),
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
           body: Form(
             key: _formKey,
